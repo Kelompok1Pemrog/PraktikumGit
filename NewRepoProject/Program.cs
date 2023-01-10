cb94211f395b0dca610eceda01fcc9fbb0414a61
rahmanhakim7 <hakimrahman261@gmail.com>
2023-01-10 07:59:09 +0700

    perubahan status

--- NewRepoProject/Program.cs
+++ NewRepoProject/Program.cs
@@ -1,1 +1,12 @@
-﻿using System 
+﻿using System;
+namespace ConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Materi Praktikum Git");
+        }
+    }
+}
